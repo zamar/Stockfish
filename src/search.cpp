cdff9f63f499643ab410574b6f29b2655f55d186
Joona Kiiski <joona.kiiski@gmail.com>
2013-05-20 20:00:54 +0100

    Do not reduce countermoves

--- src/search.cpp
+++ src/search.cpp
@@ -943,11 +943,11 @@ split_point_start: // At split points actual search starts from here
           && !dangerous
           &&  move != ttMove
           &&  move != ss->killers[0]
-          &&  move != ss->killers[1])
+          &&  move != ss->killers[1]
+          &&  move != countermoves[0]
+          &&  move != countermoves[1])
       {
           ss->reduction = reduction<PvNode>(depth, moveCount);
-          if (move == countermoves[0] || move == countermoves[1])
-              ss->reduction = std::max(DEPTH_ZERO, ss->reduction-ONE_PLY);
 
           Depth d = std::max(newDepth - ss->reduction, ONE_PLY);
           if (SpNode)
