436d8a81da5fcf72c56dcd6e33719dd48054e390
Joona Kiiski <joona.kiiski@gmail.com>
2013-06-04 23:22:10 +0100

    Aggressive LMR for cut nodes

--- src/search.cpp
+++ src/search.cpp
@@ -75,11 +75,11 @@ namespace {
   }
 
   // Reduction lookup tables (initialized at startup) and their access function
-  int8_t Reductions[2][64][64]; // [pv][depth][moveNumber]
+  int8_t Reductions[3][64][64]; // [pv][depth][moveNumber]
 
-  template <bool PvNode> inline Depth reduction(Depth d, int mn) {
+  template <bool PvNode> inline Depth reduction(Depth d, int mn, bool isCut) {
 
-    return (Depth) Reductions[PvNode][std::min(int(d) / ONE_PLY, 63)][std::min(mn, 63)];
+    return (Depth) Reductions[PvNode ? 0 : 1 + isCut][std::min(int(d) / ONE_PLY, 63)][std::min(mn, 63)];
   }
 
   size_t PVSize, PVIdx;
@@ -134,10 +134,12 @@ void Search::init() {
   // Init reductions array
   for (hd = 1; hd < 64; hd++) for (mc = 1; mc < 64; mc++)
   {
-      double    pvRed = log(double(hd)) * log(double(mc)) / 3.0;
-      double nonPVRed = 0.33 + log(double(hd)) * log(double(mc)) / 2.25;
-      Reductions[1][hd][mc] = (int8_t) (   pvRed >= 1.0 ? floor(   pvRed * int(ONE_PLY)) : 0);
-      Reductions[0][hd][mc] = (int8_t) (nonPVRed >= 1.0 ? floor(nonPVRed * int(ONE_PLY)) : 0);
+      double  pvRed = log(double(hd)) * log(double(mc)) / 3.0;
+      double allRed = 0.33 + log(double(hd)) * log(double(mc)) / 2.25;
+      double cutRed = log(double(hd)) * log(double(mc)) / 1.5;
+      Reductions[0][hd][mc] = (int8_t) ( pvRed >= 1.0 ? floor( pvRed * int(ONE_PLY)) : 0);
+      Reductions[1][hd][mc] = (int8_t) (allRed >= 1.0 ? floor(allRed * int(ONE_PLY)) : 0);
+      Reductions[2][hd][mc] = (int8_t) (cutRed >= 1.0 ? floor(cutRed * int(ONE_PLY)) : 0);
   }
 
   // Init futility margins array
@@ -530,6 +532,7 @@ namespace {
     ss->currentMove = threatMove = (ss+1)->excludedMove = bestMove = MOVE_NONE;
     ss->ply = (ss-1)->ply + 1;
     ss->futilityMoveCount = 0;
+    ss->depthFromPV = PvNode ? 0 : (ss-1)->depthFromPV + 1;
     (ss+1)->skipNullMove = false; (ss+1)->reduction = DEPTH_ZERO;
     (ss+2)->killers[0] = (ss+2)->killers[1] = MOVE_NONE;
 
@@ -886,7 +889,7 @@ split_point_start: // At split points actual search starts from here
           // Value based pruning
           // We illogically ignore reduction condition depth >= 3*ONE_PLY for predicted depth,
           // but fixing this made program slightly weaker.
-          Depth predictedDepth = newDepth - reduction<PvNode>(depth, moveCount);
+          Depth predictedDepth = newDepth - reduction<PvNode>(depth, moveCount, ss->depthFromPV & 1);
           futilityValue =  ss->staticEval + ss->evalMargin + futility_margin(predictedDepth, moveCount)
                          + Gains[pos.piece_moved(move)][to_sq(move)];
 
@@ -945,7 +948,7 @@ split_point_start: // At split points actual search starts from here
           &&  move != ss->killers[0]
           &&  move != ss->killers[1])
       {
-          ss->reduction = reduction<PvNode>(depth, moveCount);
+          ss->reduction = reduction<PvNode>(depth, moveCount, ss->depthFromPV & 1);
           if (move == countermoves[0] || move == countermoves[1])
               ss->reduction = std::max(DEPTH_ZERO, ss->reduction-ONE_PLY);
 
