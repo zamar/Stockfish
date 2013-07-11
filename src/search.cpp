c3f559fe127e19d1bb865ebc97048d3c3232fb0e
Joona Kiiski <joona.kiiski@gmail.com>
2013-07-11 20:49:13 +0100

    More aggressive reductions in the beginning of the move list

--- src/search.cpp
+++ src/search.cpp
@@ -134,8 +134,8 @@ void Search::init() {
   // Init reductions array
   for (hd = 1; hd < 64; hd++) for (mc = 1; mc < 64; mc++)
   {
-      double    pvRed = log(double(hd)) * log(double(mc)) / 3.0;
-      double nonPVRed = 0.33 + log(double(hd)) * log(double(mc)) / 2.25;
+      double    pvRed = log(double(hd)) * log(double(mc + 4)) / 3.0;
+      double nonPVRed = 0.33 + log(double(hd)) * log(double(mc + 4)) / 2.25;
       Reductions[1][hd][mc] = (int8_t) (   pvRed >= 1.0 ? floor(   pvRed * int(ONE_PLY)) : 0);
       Reductions[0][hd][mc] = (int8_t) (nonPVRed >= 1.0 ? floor(nonPVRed * int(ONE_PLY)) : 0);
   }
