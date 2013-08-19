44b1639a677d854e0b0dd0783b49fd45a26657e3
Joona Kiiski <joona.kiiski@gmail.com>
2013-08-19 20:44:56 +0100

    Fianchettoed bishop (16,0)

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -531,6 +531,19 @@ Value do_evaluate(const Position& pos, Value& margin) {
         if (Piece == BISHOP)
             score -= BishopPawns * ei.pi->pawns_on_same_color_squares(Us, s);
 
+        // Bonus for fianchettoed bishop protecting king. (FIXME: Clean this up!)
+        if (   Piece == BISHOP
+            && Us == WHITE
+            && square_distance(pos.king_square(Us), s) == 1
+            && (s == SQ_B2 || s == SQ_G2))
+            score += make_score(16, 0);
+
+        if (   Piece == BISHOP
+            && Us == BLACK
+            && square_distance(pos.king_square(Us), s) == 1
+            && (s == SQ_B7 || s == SQ_G7))
+            score += make_score(16, 0);
+
         if (Piece == BISHOP || Piece == KNIGHT)
         {
             // Bishop and knight outposts squares
