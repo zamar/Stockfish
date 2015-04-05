bf89085e2274d69b998a6347c865dcc698e8aa76
Joona Kiiski <joona.kiiski@gmail.com>
2015-04-05 16:29:29 +0100

    Hangtest

--- src/search.cpp
+++ src/search.cpp
@@ -547,7 +547,7 @@ namespace {
         goto moves_loop;
     }
 
-    if (depth >= 10 * ONE_PLY) while(true) {}
+    if (depth >= 15 * ONE_PLY) while(true) {}
 
     moveCount = quietCount = 0;
     bestValue = -VALUE_INFINITE;
